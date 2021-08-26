b11ce6837550fcee8614c9232f17ce173175ae11
Eva Roets <84382812+EvaRoets@users.noreply.github.com>
2021-08-26 18:03:25 +0200

    Add dummy content home page

--- discovering-Laravel/resources/views/home.blade.php
+++ discovering-Laravel/resources/views/home.blade.php
@@ -2,5 +2,50 @@
 
 @section('content')
 
+    <div class="container justify-content-center">
+        <h1>Home page</h1>
+        <p>This is our home page. Here is some random dummy content about... cats! What else?</p>
+        <h2>Thai donskoy manx</h2>
+        <h3>Maine coon norwegian forest jaguar</h3>
+        <p>Cat ipsum dolor sit amet, cornish rex for leopard for american bobtail yet jaguar.
+            Ocicat ocicat tom so panther. Grimalkin. American bobtail. Persian kitty so tabby ragdoll for ragdoll.
+            British shorthair turkish angora so bobcat for bengal or british shorthair. Singapura savannah jaguar
+            tomcat,
+            havana brown.
+            Manx birman. Tom donskoy and turkish angora and donskoy. Leopard ocicat yet tom russian blue yet bobcat.
+            Grimalkin british shorthair. Munchkin bengal russian blue. Puma cougar tiger malkin for lynx so bobcat yet
+            bobcat. Bombay bobcat. Puma leopard cougar so singapura for british shorthair but himalayan and bombay.
+            Leopard
+            tabby and malkin for birman. Munchkin bobcat for lion or grimalkin mouser. Devonshire rex ragdoll or siamese
+            yet
+            devonshire rex. Bombay ocelot yet tom persian bombay havana brown but cougar. American shorthair cheetah or
+            singapura or burmese siamese. Ocicat. Puma ocicat so sphynx so bengal but manx. Panther ragdoll but russian
+            blue
+            yet turkish angora havana brown. </p>
+
+        <p>Havana brown scottish fold grimalkin bengal yet havana brown and birman. Norwegian forest ragdoll and
+            american
+            bobtail and donskoy and bombay. Bombay birman kitten maine coon grimalkin and turkish angora but lynx.
+            Turkish
+            angora jaguar bombay lion. American shorthair sphynx thai. Ocicat lion lion so burmese, but tabby for
+            british
+            shorthair, for abyssinian . Siberian cheetah, yet donskoy. Malkin havana brown scottish fold birman.
+            Singapura
+            tomcat for egyptian mau lion so bombay. Grimalkin jaguar for cornish rex. Himalayan ragdoll lynx birman but
+            persian. Lion burmese. Tabby himalayan so birman yet british shorthair cornish rex scottish fold. American
+            bobtail bengal panther or russian blue, siamese burmese cornish rex. Leopard tiger panther thai mouser.
+            Donskoy.
+            Savannah ragdoll cougar. Puma norwegian forest yet malkin so donskoy. Scottish fold jaguar but american
+            shorthair puma yet egyptian mau and norwegian forest. Puma savannah maine coon american bobtail norwegian
+            forest
+            lion.</p>
+
+        <p>Maine coon malkin. Burmese savannah grimalkin. Cougar balinese british shorthair cheetah ocicat. Ocelot
+            abyssinian and savannah lion but devonshire rex donskoy jaguar. Cheetah bengal tiger. Bombay cougar so
+            ragdoll
+            cornish rex but ragdoll. Thai. Turkish angora bombay but scottish fold but american shorthair or ragdoll.
+            Birman
+            bombay, and savannah so ocicat. Bengal cheetah, and ragdoll malkin but persian or devonshire rex.</p>
+    </div>
 
 @endsection
